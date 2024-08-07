ab57da381758baa4060c37556abddfcc3d0f24f5
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2024-08-07 14:34:09 +0200

    Added menu and readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BoxHunters
+- Run away from enemy boxes but hunt other boxes!
+- You have one life so be quick!
+
+## Credits:
+- Code: Qwatrum
+- Art: Qwatrum
+- Music: -
+- Font: Anonymous Pro
---
+++ scenes/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_play_button_down():
+	pass # Replace with function body.
+
+
+
+
+func _on_credits_button_down():
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
